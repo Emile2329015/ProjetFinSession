d5df3a70861a8cf361e40509f5fd3956dea50d86
Annelevy <francialisseaugustin@gmail.com>
2025-12-08 10:13:45 -0500

    test

--- TravailFinSession/TravailFinSession/MainWindow.xaml.cs
+++ TravailFinSession/TravailFinSession/MainWindow.xaml.cs
@@ -23,7 +23,7 @@ using Windows.Foundation.Collections;
 namespace TravailFinSession
 {
     /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// An empty window that can be used on its own or navigated to within a Frame. test
     /// </summary>
     public sealed partial class MainWindow : Window
     {
